b06dae342627a69eaf092b19ba9afda047305ddf
piotrek <piotrek.bednus@gmail.com>
2025-11-23 00:13:44 +0100

    chore: Configure actual Google Analytics and AdSense IDs.

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,6 +3,6 @@ export const siteConfig = {
   domain: "www.calculate-bmi.online", // without protocol
   url: "https://www.calculate-bmi.online", // with protocol and www if applicable
   contactEmail: "support@calculate-bmi.online",
-  analyticsId: "G-XXXXXXXXXX", // Replace with actual GA4 ID
-  adsenseId: "ca-pub-XXXXXXXXXXXXXXXX", // Replace with actual AdSense ID
+  analyticsId: "G-WQN15YRX47", // Replace with actual GA4 ID
+  adsenseId: "ca-pub-7678570789023998", // Replace with actual AdSense ID
 };
